3fb235e019fd611e5c38da7212dbf99c702315c2
Alexito2060 <johsuahartwig123@gmail.com>
2022-07-15 23:42:19 -0300

    Fixed routes issue

--- src/routes/test.js
+++ src/routes/test.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.status(200).json({ message: 'Welcome to the main route' }));
 
-router.post('/register', (req, res) => res.status.json({ text: 'Welcome to the register route'}));
+router.post('/register', (req, res) => res.status(200).json({ text: 'Welcome to the register route'}));
 
-router.post('/login', (req, res) => res.status.json({ text: 'Welcome to the login route'}));
+router.post('/login', (req, res) => res.status(200).json({ text: 'Welcome to the login route'}));
 
 module.exports = router;
