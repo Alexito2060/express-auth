713040d0621094015172e9d5af97723b09111970
Alexito2060 <johsuahartwig123@gmail.com>
2022-07-15 22:39:42 -0300

    Created user model

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const config = require('config.json');
+const config = require('./config.json');
 const mongo = require('mongoose');
 const express = require('express');
 
---
+++ src/models/user.js
@@ -0,0 +1,29 @@
+const mongo = require('mongoose');
+
+const schema = new mongo.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    }, 
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    }, 
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    }, 
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongo.model('User', schema);
+
+// Bro que mas falta?
